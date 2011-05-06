f5009244b87adef378bcb84543023dc4fec99145
TANABE Ken-ichi <nabeken@tknetworks.org>
2011-05-06 14:20:21 +0900

    Fix #136 - Add Request API support

--- bin/dbee-enqueuer.rb
+++ bin/dbee-enqueuer.rb
@@ -1,35 +1,104 @@
 #!/usr/bin/env ruby
-$LOAD_PATH.unshift File.dirname(File.expand_path(__FILE__)) + '/../lib'
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
 require 'resque'
 require 'dbee/job'
+require 'facter'
+load File.dirname(__FILE__) + '/../config.rb'
 
 #
 # ./dbee-qnqueuer.rb <input> <job class>
 #
 
 def usage
-  puts "./#{File.basename(__FILE__)} <input> <job class>"
+  puts "./#{File.basename(__FILE__)} <input>"
   exit 1
 end
 
-if ARGV.size != 2
+if ARGV.size != 1
   usage
 end
 
-# 現在は1台構成なのでinputがファイルシステム上に存在するか確認
+module DBEE
+  class Enqueuer
+    attr_accessor :request_id, :http, :input, :material
+
+    def initialize(input)
+      proxy = ENV['HTTP_PROXY'] || ENV['http_proxy'] || nil
+      @http = HTTPClient.new(proxy)
+      @input = input
+      @request_url = "#{DBEE::Config::API_URL}/request/"
+      # ダウンロードジョブの関係上、任意の場所に置いたまま処理はできないので
+      # MATERIAL_DIR以下にない場合はシンボリックリンクを張る
+      @material = Pathname.new("#{DBEE::Config::MATERIAL_DIR}/#{input.basename}")
+      unless File.exist?(material)
+        @material.symlink(input)
+      end
+    end
+
+    def get_request_json
+      # request api用にJSONを生成する
+      request_json = {
+        "requester"     => Facter.value(:fqdn),
+        "material_node" => Facter.value(:fqdn),
+        "run_list" => [
+          {
+            "name" => "DBEE::Job::Download",
+            "args" => {
+              "base_url" => DBEE::Config::MATERIAL_BASE_URL
+            }
+          },
+          {
+            "name" => "DBEE::Job::Encode::IPAD",
+            "args" => {}
+          },
+          {
+            "name" => "DBEE::Job::Upload::S3",
+            "args" => {}
+          },
+          {
+            "name" => "DBEE::Job::Notification",
+            "args" => {
+              "to" => "nabeken.ipad@tknetworks.org"
+            }
+          }
+        ],
+        "program" => {
+          "name"     => "",
+          "ch"       => "",
+          "filename" => @input.basename
+        }
+      }.to_json
+    end
+
+    # 成功時はrequest_idを返す
+    def post_request
+      response = @http.post("#{@request_url}", get_request_json)
+
+      # 成功時は303 see otherが返る
+      if response.status != 303
+        raise "failed to POST request. got #{response.status}"
+      end
+      response.headers["Location"].split('/').last
+    end
+  end
+end
+
+# このコマンドを実行したホストにファイルが存在しているか確認
 if File.exist?(ARGV[0])
-  input = ARGV[0]
+  input = Pathname.new(ARGV[0])
 else
   puts "input \"#{ARGV[0]}\" no such file"
   usage
 end
 
-# 引数のjob classが実際に存在するか確認する
-begin
-  eval ARGV[1] + '.class'
-rescue NameError
-  puts "job class \"#{ARGV[1]}\" is not defined"
-  usage
-end
+enqueuer = DBEE::Enqueuer.new(input)
 
-Resque.enqueue(eval(ARGV[1]), input)
+begin
+  request_id = enqueuer.post_request
+  puts "successfully enqueued #{enqueuer.input.basename}. Request ID: #{request_id}"
+  puts "Request: #{DBEE::Config::API_URL}/request/#{request_id}"
+  exit 0
+rescue
+  puts $!
+  exit 1
+end
